f636192b1b78ecdde9089276c8d197990125364d
saddem <saddemlass@gmail.com>
2025-04-24 11:53:11 +0100

    feat: ajout de ViewerPage avec génération de description simulé

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,24 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { LoginPage } from './pages/LoginPage'
-import HomePage from './pages/HomePage'
-
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import ViewerPage from './pages/ViewerPage'
+import HistoryPage from './pages/HistoryPage'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <div className="bg-gray-950 min-h-screen text-white">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<ViewerPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/history" element={<HistoryPage />} />
+        </Routes>
+      </div>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,8 @@
+function Loader() {
+    return (
+      <div className="mt-4 text-blue-400 animate-pulse">Chargement...</div>
+    )
+  }
+  
+  export default Loader
+  
