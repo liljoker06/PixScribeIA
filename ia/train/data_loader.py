f90b2f591a55093d6563ebc26e2c83ac32ab35d8
liljoker06 <lahrimialadin@gmail.com>
2025-04-24 17:25:00 +0200

    feat: mise à jour du modèle CNN pour CIFAR-100 avec gestion des labels en français et amélioration des performances d'entraînement

--- ia/train/data_loader.py
+++ ia/train/data_loader.py
@@ -1,16 +1,27 @@
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
-def charger_cifar10(batch_size=64):
-    transform = transforms.Compose([
+def charger_cifar100(batch_size=64):
+    # Transformations avec augmentation pour le train
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize((0.5,), (0.5,))
+        transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761))
     ])
 
-    train_set = datasets.CIFAR10(root="./data", train=True, download=True, transform=transform)
-    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+    # Transfo simple pour le test
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761))
+    ])
 
-    test_set = datasets.CIFAR10(root="./data", train=False, download=True, transform=transform)
+    # Datasets
+    train_set = datasets.CIFAR100(root="./data", train=True, download=True, transform=transform_train)
+    test_set = datasets.CIFAR100(root="./data", train=False, download=True, transform=transform_test)
+
+    # Loaders
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
 
     return train_loader, test_loader
