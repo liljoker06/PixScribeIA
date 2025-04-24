2c9c2a1fa77c0f7840cd91fcf2f7435d23200538
Batyeste <batystebatybat@gmail.com>
2025-04-24 16:40:29 +0200

    route connecté + side bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,53 @@
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
-import RegisterPage from './pages/RegisterPage'
 import ViewerPage from './pages/ViewerPage'
 import HistoryPage from './pages/HistoryPage'
-import NavBar from './components/NavBar'
-import HomePage from './pages/HomePage'
-import { useState } from 'react'
+import SideBar from './components/SideBar'
+import ProtectedRoute from './components/ProtectedRoute'
 
 
-import { useAuthStore } from "./store/authStore"; 
 
+function Layout({ children }) {
+  return (
+    <div className="flex">
+      <SideBar />
+      <main className="ml-64 w-full min-h-screen">{children}</main>
+    </div>
+  )
+}
 
 function App() {
-  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
 
   return (
     <BrowserRouter>
-      <div className="bg-gray-950 min-h-screen text-white">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<ViewerPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/history" element={<HistoryPage />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+
+        {/* Routes protégées avec Layout */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Layout>
+                <ViewerPage />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/history"
+          element={
+            <ProtectedRoute>
+              <Layout>
+                <HistoryPage />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
+          
+        {/* Redirection pour les routes inconnues */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
     </BrowserRouter>
   )
 }
