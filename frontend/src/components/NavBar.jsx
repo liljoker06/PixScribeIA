51abc444323725c2355d4ac5a113a906273d47b0
liljoker06 <lahrimialadin@gmail.com>
2025-04-24 13:25:04 +0200

    refactor: remplacer le contenu des composants DescriptionDisplay, Loader et NavBar par des éléments de base

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,18 +1,27 @@
-import { Link, useLocation } from 'react-router-dom'
+// import { Link, useLocation } from 'react-router-dom'
 
-function NavBar() {
-  const { pathname } = useLocation()
+// function NavBar() {
+//   const { pathname } = useLocation()
 
+//   return (
+//     <nav className="w-full bg-gray-900 shadow-lg px-6 py-4 text-white flex justify-between items-center">
+//       <div className="text-xl font-bold">PixScribeIA</div>
+//       <div className="space-x-4 text-sm">
+//         <Link to="/" className={pathname === '/' ? 'text-blue-400 font-semibold' : 'hover:underline'}>Accueil</Link>
+//         <Link to="/history" className={pathname === '/history' ? 'text-blue-400 font-semibold' : 'hover:underline'}>Historique</Link>
+//         <Link to="/login" className={pathname === '/login' ? 'text-blue-400 font-semibold' : 'hover:underline'}>Connexion</Link>
+//       </div>
+//     </nav>
+//   )
+// }
+
+// export default NavBar
+
+
+import React from 'react'
+
+export default function NavBar() {
   return (
-    <nav className="w-full bg-gray-900 shadow-lg px-6 py-4 text-white flex justify-between items-center">
-      <div className="text-xl font-bold">PixScribeIA</div>
-      <div className="space-x-4 text-sm">
-        <Link to="/" className={pathname === '/' ? 'text-blue-400 font-semibold' : 'hover:underline'}>Accueil</Link>
-        <Link to="/history" className={pathname === '/history' ? 'text-blue-400 font-semibold' : 'hover:underline'}>Historique</Link>
-        <Link to="/login" className={pathname === '/login' ? 'text-blue-400 font-semibold' : 'hover:underline'}>Connexion</Link>
-      </div>
-    </nav>
+    <div>NavBar</div>
   )
 }
-
-export default NavBar
