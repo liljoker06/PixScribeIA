e46b8e5b264eeaa185911a62a1a14d465f944732
liljoker06 <lahrimialadin@gmail.com>
2025-04-25 17:02:16 +0200

    refactor: amélioration de l'analyse d'image avec gestion des fichiers temporaires pour la génération de descriptions

--- ia/controllers/description_controller.py
+++ ia/controllers/description_controller.py
@@ -2,17 +2,28 @@ from controllers.caption_controller import handle_image_upload
 from model.blip_model import generate_caption
 from PIL import Image
 import io
+import os
+from uuid import uuid4
 
 def analyze_image_with_caption_and_description(file_bytes: bytes, filename: str) -> dict:
     try:
-        # Appel du CNN via le contrôleur 
+        #Analyse CNN
         cnn_result = handle_image_upload(file_bytes, filename)
 
-        # Image convertie pour BLIP
-        image = Image.open(io.BytesIO(file_bytes)).convert("RGB")
-        description = generate_caption(image)
+        #Sauvegarde temporaire
+        tmp_dir = "temp_uploads"
+        os.makedirs(tmp_dir, exist_ok=True)
+        tmp_filename = f"{uuid4().hex}_{filename}"
+        tmp_path = os.path.join(tmp_dir, tmp_filename)
+        with open(tmp_path, "wb") as f:
+            f.write(file_bytes)
 
-        #Fusion des deux résultats
+        # Génération description
+        description = generate_caption(tmp_path)
+
+        # Nettoyage optionnel (tu peux le garder pour debug si besoin)
+        os.remove(tmp_path)
+
         return {
             "message": "Analyse complète réussie",
             "filename": filename,
